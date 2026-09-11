bbc17bc4873862198531ddb60f11e870d5129f3b
Daniel Coelho <danielccbcoelho@gmail.com>
2026-09-11 16:17:05 -0300

    chore: initialize Spring Boot project

---
+++ src/main/java/br/com/dwnl/spicehub/SpicehubMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package br.com.dwnl.spicehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpicehubMarketplaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpicehubMarketplaceApplication.class, args);
+    }
+
+}
