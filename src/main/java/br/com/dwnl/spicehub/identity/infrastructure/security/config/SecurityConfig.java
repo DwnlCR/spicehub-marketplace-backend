9f4505ac40005bcae9a39c06bacc9c46a7c0b833
Daniel Coelho <danielccbcoelho@gmail.com>
2026-09-21 14:40:58 -0300

    fix(security): enable CORS for frontend requests

--- src/main/java/br/com/dwnl/spicehub/identity/infrastructure/security/config/SecurityConfig.java
+++ src/main/java/br/com/dwnl/spicehub/identity/infrastructure/security/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import br.com.dwnl.spicehub.identity.infrastructure.security.jwt.JwtAuthenticati
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -24,6 +25,8 @@ public class SecurityConfig {
     ) throws Exception {
 
         http
+                .cors(Customizer.withDefaults())
+
                 .csrf(csrf -> csrf
                         .spa()
                         .ignoringRequestMatchers(
